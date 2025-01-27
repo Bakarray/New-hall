961cc80de2e2726d9d1c6ed128703c29f8445624
Bakarray <magnificentzeem@gmail.com>
2025-01-27 21:12:52 +0100

    set debug to falase

--- new_hall/settings.py
+++ new_hall/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-local-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://mysite-h8re.onrender.com', '127.0.0.1', 'localhost']
 
